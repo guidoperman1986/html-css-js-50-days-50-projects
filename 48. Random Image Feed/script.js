72c40bb8e7443d31d7638bd134e46719e03f4aec
Guido <38866056+guidoperman1986@users.noreply.github.com>
2023-02-20 11:55:04 -0300

    some changes on image feed

--- 48. Random Image Feed/script.js
+++ 48. Random Image Feed/script.js
@@ -1,10 +1,30 @@
 const container = document.querySelector('.container');
+const input = document.querySelector('input')
 const unsplashURL = 'https://source.unsplash.com/random/';
-const rows = 5;
+let rows = 5;
 
-for (let i = 0; i < rows * 3; i++) {
-    const img = document.createElement('img');
-    img.src = `${unsplashURL}30${i}x300`;
-
-    container.appendChild(img)    
+function showImages() {
+    for (let i = 0; i < rows * 3; i++) {
+        const img = document.createElement('img');
+        img.src = `${unsplashURL}30${i}x300`;
+    
+        container.appendChild(img)    
+    }    
 }
+
+input.addEventListener('keyup', ({keyCode, target})=> {
+    if (keyCode === 13) {
+        const { value } = target;
+        if (isNaN(value)) return;
+
+        rows = value;
+    }
+
+    container.innerHTML = ''   
+    showImages()
+})
+
+showImages();
+
+
+
